71daeef8c8e382873d1d92b20429852bd2d8754b
alexdeakyne <deaky007@umn.edu>
2019-09-05 10:55:58 -0500

    Second Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 15F;
+    public GameObject player;
+
+    void Update()
+    {
+        if (GvrControllerInput.IsTouching && GvrControllerInput.ClickButton)
+        {
+            print("touch");
+            Vector2 touchPos = GvrControllerInput.TouchPos;
+            float velocityZ = (1 - (touchPos.y * 2f)) * .2f;
+            player.transform.Translate(velocityZ * transform.forward * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
---
+++ Assets/Scripts/Object_Loop.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Loop : MonoBehaviour {
+    private int currFrame = 0;
+    private Transform[] allChildren;
+    private float timeLog;
+    public float refreshRate = 2.5f/30f;
+
+	// Use this for initialization
+	void Start () {
+        allChildren = new Transform[transform.childCount];
+        int i = 0;
+        foreach(Transform child in transform)
+        {
+            allChildren[i] = child;
+            i++;
+
+        }
+        allChildren[0].gameObject.SetActive(true);
+        for (i = 1; i < transform.childCount; i++)
+        {
+            allChildren[i].gameObject.SetActive(false);
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeLog = timeLog + Time.deltaTime;
+        if (refreshRate < timeLog)
+        {
+            allChildren[currFrame].gameObject.SetActive(false);
+            currFrame = (currFrame + 1) % transform.childCount;
+            allChildren[currFrame].gameObject.SetActive(true);
+            timeLog = 0.0f;
+        }
+		
+	}
+}
